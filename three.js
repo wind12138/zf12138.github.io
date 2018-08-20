8e080521e9b6bb8ea00152bfb7e2846ea56b6e53
china_zf <32032473+wind12138@users.noreply.github.com>
2018-08-20 14:30:24 +0800

    添加了多属性动画，bug会出现多次end_function

--- three.js
+++ three.js
@@ -21,7 +21,6 @@ function init_bannar(b_div,b_img) {
     var bannar_fistimg = document.querySelector("#bannar div img");
     var pro_bar = document.querySelector(".pro_bar div");
     var all_h = document.querySelectorAll(".all_h");
-
     for(var i = 0;i < 5;i++) {
         b_img[i].src = imgs[0].src;
         if(i != 0){
@@ -30,20 +29,25 @@ function init_bannar(b_div,b_img) {
         }
     }
     i = null;
-
     var all_h_0 = new css_animate(all_h[0],{transform:44,opacity:1},1);
     all_h_0.css_animate_init();
     all_h_0 = null;
     for(var s = 0;s<all_h[0].children.length;s++){
         var all_h_children = all_h[0].children[s];
         var all_h_children_attr = parseInt(window.getComputedStyle(all_h_children,null)["left"]);
-        var all_h_children_animate = new css_animate(all_h_children,{left:-all_h_children_attr,opacity:1},6);
-        all_h_children_animate.css_animate_init(function(){
-            console.log(all_h_children_attr);
-        });
+        console.log(all_h_children_attr);
+        var all_h_children_animate = new css_animate(all_h_children,{opacity:1,left:-all_h_children_attr},6);
+        if(s == 1){all_h_children_animate.css_animate_init();}
+        else {all_h_children_animate.css_animate_init(function(){
+			setTimeout(function () {
+                all_h[0].style.cssText = "opacity:0;transform:rotate(-45deg)";
+                all_h[0].children[0].style.cssText = "opacity:0;left:100%";
+                all_h[0].children[1].style.cssText = "opacity:0;left:-80%";
+            },3000);
+		});}
     }
     all_h_children_animate = null;
-	bannar_animate(b_div,b_img,bannar_fistimg,pro_bar);
+	bannar_animate(b_div,b_img,bannar_fistimg,pro_bar,all_h);
 }
 
 /*编写对象封装动画效果*/
@@ -55,12 +59,21 @@ function css_animate (name,attr,speed) {
 
 css_animate.prototype = {
     css_animate_init:function(end_function){
+        var n = 0;
+        var attr_length = Object.getOwnPropertyNames(this.attr).length;
         for(var p in this.attr){
 			var string_p = p.toString();
 			var inita_value = parseInt(window.getComputedStyle(this.name,null)[string_p]);
-            this.css_animate_strat(this.name, string_p, this.attr[p], this.speed, inita_value, end_function);
+			if(n == attr_length-1){
+                this.css_animate_strat(this.name,string_p,this.attr[p],this.speed,inita_value,end_function);
+            }
+            else {
+                this.css_animate_strat(this.name,string_p,this.attr[p],this.speed,inita_value);
+            }
+			n++;
         }
     },
+    /* BUG----opacity，transform使用end_function会出现bug-----目前弥补方式是将opacity和transform写在前面 */
     css_animate_strat:function(css_name,css_attr,css_time,css_speed,css_value,end_function){
         var t = 0;
         function css_strat(){
@@ -68,12 +81,12 @@ css_animate.prototype = {
                 (css_value < 1)?t++:t--;
                 css_name.style[css_attr] = (css_value+t*0.01).toString();
                 if(css_value < 1){if(t*0.01 <= 1){requestAnimationFrame(css_strat);}else{end_function();}}
-                else{if(t*0.01 >= -1){requestAnimationFrame(css_strat);}else {end_function();}}
+                else{if(t*0.01 >= -1){requestAnimationFrame(css_strat);}else{end_function();}}
             }
             if(css_attr == "transform"){
                 (css_time>0)?t++:t--;
                 css_name.style[css_attr] = "rotate("+(-45 + t*css_speed).toString()+"deg)";
-                if(css_time>0){if(t*css_speed <= css_time)requestAnimationFrame(css_strat);}
+                if(css_time>0){if(t*css_speed <= css_time){requestAnimationFrame(css_strat);}else{end_function();}}
                 else {if(t*css_speed >= css_time){requestAnimationFrame(css_strat);}else{end_function();}}
             }
             else{
@@ -88,7 +101,7 @@ css_animate.prototype = {
 }
 
 /*编写轮播的动画*/
-function bannar_animate(ba_div,ba_img,fim,pro){
+function bannar_animate(ba_div,ba_img,fim,pro,all){
     var five_n = 0;
     var pro_width = parseInt(document.defaultView.getComputedStyle(pro.parentNode,null)["width"]);
     var pro_left = parseInt(document.defaultView.getComputedStyle(pro,null)["left"]);
@@ -100,16 +113,15 @@ function bannar_animate(ba_div,ba_img,fim,pro){
             if(pro_left > parseInt(pro_width*3/4)){
                 pro.style["left"] = "0px";
             }
-
         });
         if(img_n>=imgs.length){img_n = 0;}
         fim.src = imgs[(img_n>imgs.length?0:img_n)].src;
-        FB_animate(five_n,ba_div,ba_img,fim,pro);
+        FB_animate(five_n,ba_div,ba_img,fim,pro,all);
     },4000);
 }
 
 /*编写五个小的轮播*/
-function FB_animate(n,di,im,fim,pro){
+function FB_animate(n,di,im,fim,pro,all){
     FB = null;
     var FB_height = parseInt(window.getComputedStyle(di[n],null)["height"]);
     var FB = new css_animate(di[n],{height:-FB_height},10);
@@ -125,6 +137,6 @@ function FB_animate(n,di,im,fim,pro){
             }
         });
         n++;
-        if(n<di.length)FB_animate(n,di,im,fim,pro);
+        if(n<di.length)FB_animate(n,di,im,fim,pro,all);
     },500);
 }
